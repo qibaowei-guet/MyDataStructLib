15f13ce1c671f3c4791d4c7d2c37243769b599db
qibaowei-guet <2598170371@qq.com>
2018-01-06 22:08:11 +0800

    add staticLinkList

---
+++ StaticLinkList.h
@@ -0,0 +1,19 @@
+#ifndef STATICLINKLIST_H
+#define STATICLINKLIST_H
+
+#include "LinkList.h"
+
+namespace MyDataStructLib {
+
+template < typename T >
+class StaticLinkList
+{
+public:
+    StaticLinkList() {}
+};
+
+
+};
+
+
+#endif // STATICLINKLIST_H
